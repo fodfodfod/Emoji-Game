64e5812b6d28b6585fc285083261a18313396e15
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-02-04 17:10:00 -0500

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,29 @@
+public class Person {
+    private String name;
+    private String gender;
+    private String symbol;
+    private String description;
+    private int points;
+
+
+    public Person(String n, String g, String sym, String d){
+        name = n;
+        gender = g;
+        symbol = sym;
+        description = d;
+        points = 0;
+        
+
+    }
+    public String getName(){
+        return name;
+    }
+
+    public String getSymbol(){
+        return symbol;
+    }
+
+    public void addPoint(){
+        points++;
+    }
+}
